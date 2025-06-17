15b320bc4db33e1fb321bcad8fda1fa4bc0ed7bb
hekkort <harryvanderknaap@gmail.com>
2025-06-17 08:50:12 +0200

    added more stuff to run_python

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,7 +1,32 @@
 import os
+from pathlib import Path
+import subprocess
 
 def run_python_file(working_directory, file_path):
-    abs_file_path = os.path.join(os.path.abspath(working_directory), file_path)
 
-    if os.path.commonpath([abs_file_path, working_directory]) != working_directory:
+    working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.join(working_directory, file_path)
+    path = Path(abs_file_path)
+    resolved_path = path.resolve()
+    resolved_path = str(resolved_path)
+    
+    # print(resolved_path[-3:])
+    # print(resolved_path)
+    # print(os.path.exists(resolved_path))
+
+    if os.path.commonpath([resolved_path, working_directory]) != working_directory:
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    elif not os.path.exists(resolved_path):
+        return f'Error: File "{file_path}" not found.'
+    
+    elif resolved_path[-3:] != ".py":
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    else:
+        try:
+            x = subprocess.run(["python3", resolved_path], capture_output=True, timeout=30)
+            string = str(x.stdout) + str(x.stderr) + str(x.returncode)
+            return string
+        except Exception as e:
+            return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 #print(get_files_info("calculator", "."))
 #print(get_files_info("calculator", "pkg"))
 #print(get_files_info("calculator", "/bin"))
@@ -7,6 +7,10 @@ from functions.write_file import write_file
 #print(get_file_content("calculator", "main.py"))
 #print(get_file_content("calculator", "pkg/calculator.py"))
 #print(get_file_content("calculator", "/bin/cat"))
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+# print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+# print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+# print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+print(run_python_file("calculator", "main.py"))
+print(run_python_file("calculator", "tests.py"))
+print(run_python_file("calculator", "../main.py"))
+print(run_python_file("calculator", "nonexistent.py"))
